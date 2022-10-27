b1978cbb354c643d99f59930488bd49a6128f9b2
blackatz <ksenya.noskova.98@inbox.ru>
2022-10-27 19:40:11 +0300

    new hard

---
+++ 19HARD/Program.cs
@@ -0,0 +1,31 @@
+﻿// напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом
+// 14212 - нет
+// 12821 - да
+
+Console.WriteLine("Введите число");
+
+int number = int.Parse(Console.ReadLine()!);
+int final = 0;
+
+int Mirror(int number)
+{
+    while (number > 0)
+    {   
+        int number1 = number%10;
+        number = number/10;
+        final = final*10 + number1;
+    }
+    Console.Write(final);
+    return final;
+
+}
+
+if (Mirror(number) == number)
+{
+    Console.WriteLine("   Число является палиндромом");
+}
+else
+{
+    Console.WriteLine("   Число не является палиндромом");
+}
+
